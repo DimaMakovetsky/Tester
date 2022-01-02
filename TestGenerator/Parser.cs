8b1419f57c20242f611f90c8bf5788f3740f4941
DimaMakovetsky <mister.chuppa@mail.ru>
2022-01-02 18:22:12 +0300

    Console

---
+++ TestGenerator/Parser.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Threading.Tasks;
+
+namespace TestGenerator
+{
+    public class Parser
+    {
+        public Files GetFileData(string source)
+        {
+            CompilationUnitSyntax tree = CSharpSyntaxTree.ParseText(source).GetCompilationUnitRoot();
+            List<Classes> classes = new List<Classes>();
+            foreach (var classDeclaration in tree.DescendantNodes().OfType<ClassDeclarationSyntax>())
+            {
+                Classes data = GetClassData(classDeclaration);
+                if (data.Methods.Count != 0)
+                {
+                    classes.Add(data);
+                }
+            }
+
+            return new Files(classes);
+        }
+
+        private Classes GetClassData(ClassDeclarationSyntax syntax)
+        {
+
+            List<Constructors> ctors = new List<Constructors>();
+            foreach (var ctor in syntax.DescendantNodes()
+                .OfType<ConstructorDeclarationSyntax>()
+                .Where((cd) => cd.Modifiers
+                .Any((c) => c.IsKind(SyntaxKind.PublicKeyword))))
+            {
+                ctors.Add(GetConstructorData(ctor));
+            }
+
+            List<Methods> methods = new List<Methods>();
+            foreach (var method in syntax.DescendantNodes()
+                .OfType<MethodDeclarationSyntax>()
+                .Where((md) => md.Modifiers
+                .Any((m) => m.IsKind(SyntaxKind.PublicKeyword))))
+            {
+                methods.Add(GetMethodData(method));
+            }
+
+            return new Classes(syntax.Identifier.ValueText, ctors, methods);
+        }
+
+        private Constructors GetConstructorData(ConstructorDeclarationSyntax syntax)
+        {
+            Dictionary<string, string> parameters = new Dictionary<string, string>();
+            foreach (var param in syntax.ParameterList.Parameters)
+            {
+                parameters.Add(param.Identifier.Text, param.Type.ToString());
+            }
+
+            return new Constructors(syntax.Identifier.Text, parameters);
+        }
+
+        private Methods GetMethodData(MethodDeclarationSyntax syntax)
+        {
+            Dictionary<string, string> parameters = new Dictionary<string, string>();
+            foreach (var param in syntax.ParameterList.Parameters)
+            {
+                parameters.Add(param.Identifier.Text, param.Type.ToString());
+            }
+
+            return new Methods(syntax.Identifier.Text, parameters, syntax.ReturnType.ToString());
+        }
+    }
+}
+
--- Tester/Program.cs
+++ Tester/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace Tester
@@ -10,6 +10,51 @@ namespace Tester
     {
         static void Main(string[] args)
         {
+            string filesDirectory = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName + "\\Files";
+            Console.WriteLine(filesDirectory);
+
+            string pathToGenerated = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.Parent.FullName + "\\GeneratedTests\\Tests";
+            Console.WriteLine(pathToGenerated);
+            if (!Directory.Exists(filesDirectory))
+            {
+                Console.WriteLine($"Couldn't find directory {filesDirectory}");
+                return;
+            }
+            if (!Directory.Exists(pathToGenerated))
+            {
+                Directory.CreateDirectory(pathToGenerated);
+            }
+
+            var allFiles = Directory.GetFiles(filesDirectory);
+            int maxThreads = GetNumberFromUser();
+            var files = from file in allFiles
+                        where file.Substring(file.Length - 3) == ".cs"
+                        select file;
+            TestsGenerationManager manager = new TestsGenerationManager();
+
+            Task task = manager.Generate(files, pathToGenerated, maxThreads);
+            task.Wait();
+            Console.WriteLine("end.");
+        }
+
+        private static int GetNumberFromUser()
+        {
+            int threadsNum;
+            do
+            {
+                Console.WriteLine("Write max number of threads: ");
+                string threads = Console.ReadLine();
+                try
+                {
+                    threadsNum = int.Parse(threads);
+                    return threadsNum;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("\"" + threads + "\"" + " is not a number.");
+                    continue;
+                }
+            } while (true);
         }
     }
 }
